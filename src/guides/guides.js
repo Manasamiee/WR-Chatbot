2406728af56eb1c86a87205ca3b7a891eb0fb502
Mana Samiee <133782854+Manasamiee@users.noreply.github.com>
2024-03-11 14:49:17 +0100

    Update guides.js

--- src/guides/guides.js
+++ src/guides/guides.js
@@ -1,6 +1,6 @@
 export const guides = [
-    { title: "Facts", description: "Please share detailed information about your symptoms, medical history, and your current condition." },
-    { title: "Feelings", description: "Please feel free to share your thoughts, fears, or concerns about your symptoms and your condition." },
-    { title: "Fears", description: "Please discuss how your symptoms affect your daily life. How has your condition impacted your work, home life, or social activities?" },
-    { title: "Future", description: "Please share your goals for treatment and any particular outcomes you hope to achieve. Feel free to click the Summarise button when you are ready to wrap up the conversation." },
+    { title: "Facts", description: "Symptoms, medical history, and your current condition." },
+    { title: "Feelings", description: "Concerns or thoughts about your symptoms and your condition." },
+    { title: "Fears", description: "Impact of your condition on your daily life" },
+    { title: "Future", description: "Expectations, hopes, and goals you have for the future" },
   ];
